0ccd6053aa59dbff57a3fdc0e38b5438e505da5f
Phuchipat <38816892+Phuchipat@users.noreply.github.com>
2024-02-26 23:39:38 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+-- ░██████╗░░██████╗░█████╗░███╗░░██╗██╗░█████╗░░░░███╗░░██╗███████╗████████╗
+-- ██╔════╝░██╔════╝██╔══██╗████╗░██║██║██╔══██╗░░░████╗░██║██╔════╝╚══██╔══╝
+-- ██║░░██╗░╚█████╗░██║░░██║██╔██╗██║██║██║░░╚═╝░░░██╔██╗██║█████╗░░░░░██║░░░
+-- ██║░░╚██╗░╚═══██╗██║░░██║██║╚████║██║██║░░██╗░░░██║╚████║██╔══╝░░░░░██║░░░
+-- ╚██████╔╝██████╔╝╚█████╔╝██║░╚███║██║╚█████╔╝██╗██║░╚███║███████╗░░░██║░░░
+-- ░╚═════╝░╚═════╝░░╚════╝░╚═╝░░╚══╝╚═╝░╚════╝░╚═╝╚═╝░░╚══╝╚══════╝░░░╚═╝░░░
+-- 💬 Resource : Gsonic.GlobalSystem By Gsonic Team l https://discord.gg/7JmRrKFmMu
+-- ⛔️ Warning: ทรัพยากรนี้ทำมาเพื่อแจก ห้ามจำหน่ายโดยเด็ดขาด
+
+-- ☕ Thanks For Support Script
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Gsonic & Azael Dev (https://gsonic.net/) (https://www.azael.dev/ )'
+description 'GlobalSystem'
+version '1.0.0'
+url 'https://gsonic.net/GlobalSystem/'
+
+lua54 'yes'
+
+server_scripts {
+	"Setting/Setting.lua",
+	"Source/Main.Server.lua",
+}
